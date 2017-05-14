afeef575b61fc5aa28d7311dd3b80b2f7326ef53
huyhoang17 <hoangphan0710@gmail.com>
2017-05-14 19:05:08 +0700

    fix root urls

--- pjweb/urls.py
+++ pjweb/urls.py
@@ -1,24 +1,17 @@
-"""pjweb URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
+
+from newletters.views import HomeView
 
 urlpatterns = [
+    url(r'^$', HomeView.as_view(), name="home"),
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^jobs/', include('job.urls')),
+    url(r'^developers/', include('accounts.urls')),
+    url(r'^companies/', include('companys.urls')),
 ]
 
 if settings.DEBUG:
@@ -27,3 +20,7 @@ if settings.DEBUG:
     urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
     ]
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
