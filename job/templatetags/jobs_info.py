1f33086536429f435b0e40a5aa171638072b0d8f
huyhoang17 <hoangphan0710@gmail.com>
2017-05-21 23:17:08 +0700

    add custom static, improve FE

--- job/templatetags/jobs_info.py
+++ job/templatetags/jobs_info.py
@@ -3,7 +3,17 @@ from django import template
 register = template.Library()
 
 
-@register.filter(is_safe=True)
-def dot_replace(value):
-    value = value.replace('|', '. ')
-    return value
+@register.filter()
+def convert_to_html(raw_string):
+    output = []
+    for line in raw_string.split("|"):
+        output.append("<li>{}</li>".format(line.strip('-')))
+    return "".join(output)
+
+
+@register.filter()
+def html_inline(raw_string):
+    if raw_string is None:
+        return "<small style='color: #000;'>Unknown</small>"
+    output = raw_string.split("|")
+    return "<small style='color: #000;'>{}</small>".format("".join(output))
