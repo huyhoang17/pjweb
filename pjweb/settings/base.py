a896ed62d5f0d2281765c26c66f16e4b1b4baba7
Hung Nguyen Viet <hvn@familug.org>
2017-06-13 12:14:24 +0700

    Use CircleCi 2.0, set test db to postgres

--- pjweb/settings/base.py
+++ pjweb/settings/base.py
@@ -86,6 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'pjweb',
+        'USER': os.environ.get('POSTGRES_USER', ''),
         'HOST': 'localhost',  # '127.0.0.1',
         'PORT': '',  # '5432',
     }
